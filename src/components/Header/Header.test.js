9a70046ac04ba16426cf663f069d8ae00ac85bf4
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-29 12:18:39 +0100

    Mocked the @fortawesome/react-fontawesome module

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,6 +2,12 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Header from "./Header";
 
+jest.mock("@fortawesome/react-fontawesome", () => ({
+  FontAwesomeIcon: () => {
+    return <svg />;
+  },
+}));
+
 describe("Given a Header component", () => {
   describe("When it renders", () => {
     test("Then it should show the logo", () => {
